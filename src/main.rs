92bc64cc303b3700cb864f9b5e6f89a8360dce8d
minejerik <jaanerikf@gmail.com>
2023-10-02 10:12:09 -0400

    getting user input and url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use reqwest::blocking::Client;
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let client = Client::new;
+
+
+    let url: String;
+
+    if args.len() != 2 {
+        url = "ipinfo.io/json".to_string();
+    } else {
+        let ip = &args[1];
+        url = format!("ipinfo.io/{ip}/json");
+    }
+
+    println!("{}", url);
 }
